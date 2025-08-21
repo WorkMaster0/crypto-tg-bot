2f86f595bb63fd242c6f630c1bac9d2b83d13a5a
WorkMaster0 <stasvit058@gmail.com>
2025-08-21 22:53:15 +0200

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,1 +1,7 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()  # Завантажує змінні з .env файлу
+
+TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+TELEGRAM_CHANNEL_ID = os.getenv('TELEGRAM_CHANNEL_ID')
