439130f3ff9f8ecc97fb0e7655387d570a6fd3b5
WorkMaster0 <stasvit058@gmail.com>
2025-08-25 09:39:16 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-from app.bot import main
+from app.bot import start_polling
+import app.handlers  # noqa: F401  (реєструє хендлери)
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    start_polling()
