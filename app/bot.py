7912d53fcebbe293a62bbecb3b66fecc37970935
WorkMaster0 <stasvit058@gmail.com>
2025-08-25 10:04:09 +0200

    Update bot.py

--- app/bot.py
+++ app/bot.py
@@ -1,8 +1,4 @@
 import telebot
 from app.config import TELEGRAM_BOT_TOKEN
 
-bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN, parse_mode="HTML")
-
-def start_polling():
-    print("Bot is running...")
-    bot.infinity_polling(timeout=60, long_polling_timeout=60)
+bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN, parse_mode="Markdown")
