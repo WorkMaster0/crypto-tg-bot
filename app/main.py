95f1a91c0a8059a1cf380ba6a686cee7cb0ed1e4
WorkMaster0 <stasvit058@gmail.com>
2025-08-26 19:23:17 +0200

    Delete app/main.py

--- app/main.py
+++
@@ -1,9 +0,0 @@
-from app.bot import bot
-import app.handlers  # завантажує всі команди
-
-def main():
-    print("✅ Bot is running...")
-    bot.infinity_polling()
-
-if __name__ == '__main__':
-    main()
