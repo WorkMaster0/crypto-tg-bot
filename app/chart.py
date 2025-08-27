faf3a84016ead7e08eba8911de2cc409b4042cc1
WorkMaster0 <stasvit058@gmail.com>
2025-08-27 19:51:34 +0200

    Create chart.py

---
+++ app/chart.py
@@ -0,0 +1,31 @@
+import mplfinance as mpf
+import matplotlib.pyplot as plt
+from io import BytesIO
+from app.analytics import get_ohlcv
+
+def plot_candles(symbol, timeframe='1h'):
+    """Побудувати свічковий графік"""
+    df = get_ohlcv(symbol, timeframe)
+    if df.empty:
+        return None
+    
+    # Підготовка даних
+    df.set_index('timestamp', inplace=True)
+    
+    # Створення графіка
+    fig, axes = mpf.plot(
+        df,
+        type='candle',
+        style='charles',
+        volume=True,
+        returnfig=True,
+        figsize=(10, 8)
+    )
+    
+    # Збереження в буфер
+    buf = BytesIO()
+    plt.savefig(buf, format='png', dpi=100, bbox_inches='tight')
+    buf.seek(0)
+    plt.close(fig)
+    
+    return buf
