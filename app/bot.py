165f630882ba839b2c8539b770eaf77004582195
WorkMaster0 <stasvit058@gmail.com>
2025-09-08 20:11:30 +0200

    Delete app directory

--- app/bot.py
+++
@@ -1,8 +0,0 @@
-from telebot import TeleBot
-from app.config import TELEGRAM_BOT_TOKEN
-
-if not TELEGRAM_BOT_TOKEN:
-    raise RuntimeError("TELEGRAM_BOT_TOKEN is not set")
-
-# HTML безпечніший за Markdown щодо символів
-bot = TeleBot(TELEGRAM_BOT_TOKEN, parse_mode="HTML")
