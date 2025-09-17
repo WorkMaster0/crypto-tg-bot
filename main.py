b5123d700e4beae268fb6ecae0fe62325cf4daf4
WorkMaster0 <stasvit058@gmail.com>
2025-09-17 23:24:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ CHAT_ID = os.getenv("CHAT_ID", "")
 WEBHOOK_URL = os.getenv("WEBHOOK_URL", "")
 PORT = int(os.getenv("PORT", "5000"))
 
-TOP_LIMIT = int(os.getenv("TOP_LIMIT", "10"))
+TOP_LIMIT = int(os.getenv("TOP_LIMIT", "100"))
 SCAN_INTERVAL_MINUTES = int(os.getenv("SCAN_INTERVAL_MINUTES", "1"))
 EMA_SCAN_LIMIT = int(os.getenv("EMA_SCAN_LIMIT", "500"))
 PARALLEL_WORKERS = int(os.getenv("PARALLEL_WORKERS", "6"))
